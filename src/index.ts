e7b8e037c0dddc264b50b65c1aad1ae24d314450
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2024-12-29 22:33:42 +0530

    socket working with redis

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 import { createServer } from 'http';
 import morgan from 'morgan';
 import { Server } from 'socket.io';
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import { Redis } from "ioredis";
 
 config();
 
@@ -12,12 +14,27 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(morgan('dev'));
 
+const redis = new Redis({
+    host : "localhost",
+    port : 6379
+});
+
 const server = createServer(app);
-const io = new Server(server)
+const io = new Server(server,{
+    cors: {
+        origin: '*'
+    },
+    adapter: createAdapter(redis)
+})
+
+
 
 io.on('connection',(socket) =>{
     console.log(`User Connected: ${socket.id}`);
-    io.emit('message', 'Welcome to the chat');
+    socket.on('msg',(data) =>{
+        console.log(data);
+        io.emit('msg',data);
+    })
 })
 
 app.get('/', (req: Request, res: Response) => {res.json({ message: 'Hello World!' });});    
